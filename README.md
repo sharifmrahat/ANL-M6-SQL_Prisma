5e7b531166a87f581fdd27a56e5b72f0a35b537e
sharifmrahat <sharifmrahat@gmail.com>
2023-10-04 21:02:22 +0600

    resource updated

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# ANL-M6-SQL_Prisma
+## Apollo Next Level | M-6 : M-28---48
+
+### Mission-6: Be a SQL Super Star & Prisma Pro
+
+- Module 28 : Introduction to Database Design
+- Module 29: Normalization and Core Concepts
+- Module 30: SQL Introduction
+- Module 31: Advanced SQL queries and Optimization
+- Module-32: Prisma
+- Module-33: Database operations & Development
+- Module-34: Assignment-7
+- Module-35: 
+- Module-36: 
+- Module-37:
+- Module-38:
+- Module-39:
+- Module-40:
+- Module-41: 
+- Module-42:
+- Module-43:
+- Module-44:
+- Module-45:
+- Module-46:
+- Module-47:
+- Module-48:
+
+
+## Important Links
+
+- [Prisma Docs](https://www.prisma.io/docs)
+- [Data Model](https://www.prisma.io/docs/concepts/components/prisma-schema/data-model)
+- [CRUD with Prisma Client](https://www.prisma.io/docs/concepts/components/prisma-client/crud)
+- [Filtering and Sorting](https://www.prisma.io/docs/concepts/components/prisma-client/filtering-and-sorting)
+- [Pagination](https://www.prisma.io/docs/concepts/components/prisma-client/pagination)
+- [Aggregation, grouping, and summarizing](https://www.prisma.io/docs/concepts/components/prisma-client/aggregation-grouping-summarizing)
+- [Transactions and batch queries](https://www.prisma.io/docs/concepts/components/prisma-client/transactions)
+- [Raw database access](https://www.prisma.io/docs/concepts/components/prisma-client/raw-database-access)
+- [Prisma Studio](https://www.prisma.io/docs/concepts/components/prisma-studio)
+- [Team development with Prisma Migrate](https://www.prisma.io/docs/guides/migrate/developing-with-prisma-migrate/team-development)
+>>>>>>> 48bed44 (resource updated)
---
+++ postgresql-prisma/project-setup/prisma/migrations/20230907152814_relation/migration.sql
@@ -0,0 +1,74 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Category` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Profile` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Category";
+
+-- DropTable
+DROP TABLE "Post";
+
+-- DropTable
+DROP TABLE "Profile";
+
+-- DropTable
+DROP TABLE "User";
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "age" INTEGER,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "profiles" (
+    "id" SERIAL NOT NULL,
+    "bio" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "profiles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "posts" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "authorId" INTEGER NOT NULL,
+    "categoryId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "posts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "profiles_userId_key" ON "profiles"("userId");
+
+-- AddForeignKey
+ALTER TABLE "profiles" ADD CONSTRAINT "profiles_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "posts" ADD CONSTRAINT "posts_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "posts" ADD CONSTRAINT "posts_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
