45d029ebf70be86e7d44079309cb5ee7c703ae0e
sharifmrahat <sharifmrahat@gmail.com>
2023-10-04 21:10:58 +0600

    resource updated

--- src/app.ts
+++
@@ -1,9 +0,0 @@
-import express, { Application } from "express";
-import cors from "cors";
-
-const app: Application = express();
-app.use(express.json());
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-
-export default app;
